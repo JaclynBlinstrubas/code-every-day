b117c8ee5c1532afbca9fe022e44b9be3d00d547
JBlin1819 <jaclyn.blinstrubas@gmail.com>
2021-08-11 21:39:08 -0400

    Day 1/100

--- countingSheep.js
+++ countingSheep.js
@@ -1,12 +1,8 @@
-const arrayOfSheep = [true,  true,  true,  false,
-    true,  true,  true,  true ,
-    true,  false, true,  false,
-    true,  false, false, true ,
-    true,  true,  true,  true ,
-    false, false, true,  true]
 
 function countSheeps(arrayOfSheep){
+   
     let sheep = 0
+
     for (let i = 0; i < arrayOfSheep.length; i++) {
         if (arrayOfSheep[i]) sheep += 1
     }
